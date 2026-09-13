7eb88e7545aa37b5f38c012720d355fe549db11a
Jorge Leiva <jorge.leiva.l@gmail.com>
2026-09-13 19:34:25 -0300

    feat: implementar componentes Register y Login con manejo de estados y validaciones de contraseña

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hito-1-pizzeria-mamma-mia/",
+  base: "/hito-2-pizzeria-mamma-mia/",
 })
